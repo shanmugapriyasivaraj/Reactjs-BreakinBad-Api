7f9fc3e13b17760ad254018ef46f8f5321fb7069
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-11 16:31:54 +0530

    search input adding

--- src/App.js
+++ src/App.js
@@ -3,25 +3,29 @@ import "./App.css";
 import Header from "./components/Ui/Header";
 import axios from "axios";
 import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/Ui/Search";
+
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
       );
-      console.log(result.data);
+      // console.log(result.data);
       setItems(result.data);
       setIsLoading(false);
     };
 
     fetchItems();
-  }, []);
+  }, [query]);
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(e) => setQuery(e)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
--- src/components/characters/CharacterGrid.jsx
+++ src/components/characters/CharacterGrid.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Spinner from "../Ui/Spinner";
+import CharacterItem from "./CharacterItem";
 
 const CharacterGrid = ({ items, isLoading }) => {
   return isLoading ? (
-    <h1>Loading...</h1>
+    <Spinner />
   ) : (
     <section className="cards">
       {items.map((item) => (
-        <h1 key={item.char_id}>{item.name}</h1>
+        <CharacterItem key={item.char_id} item={item}></CharacterItem>
       ))}
     </section>
   );
