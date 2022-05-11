61c05acd63f7c1d69520aa321c9492bab9778eea
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-11 15:19:47 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Ui/Header";
-
+import axios from "axios";
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
   return (
     <div className="container">
       <Header />
